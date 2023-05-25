072ad0ac8f024bd45517c2210fb4aef1b3c0a4e0
kowais915 <kowais915@gmail.com>
2023-05-25 23:34:16 +0500

    :construction: created footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './comps/navbar/navbar.component';
+import { FooterComponent } from './comps/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
