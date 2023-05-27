8f06b20c3ee82b65fb48e4caafe305495033ab62
kowais915 <kowais915@gmail.com>
2023-05-27 07:15:20 +0500

    :sparkles: done with the UI: added modal, job cards

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './comps/navbar/navbar.component';
 import { FooterComponent } from './comps/footer/footer.component';
+import { HeaderComponent } from './comps/header/header.component';
+import { MainComponent } from './comps/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
